64c9a76efeb649aa9ae5a47de8a04d7c7b08da97
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-10-09 03:23:37 +0600

    Installed firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA6PBoZFltbCba-Tu8ZJu2wHTYLy62-dkE",
+  authDomain: "earena-22aa4.firebaseapp.com",
+  projectId: "earena-22aa4",
+  storageBucket: "earena-22aa4.appspot.com",
+  messagingSenderId: "11697375195",
+  appId: "1:11697375195:web:549d7c6319120d0b9949fe",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
