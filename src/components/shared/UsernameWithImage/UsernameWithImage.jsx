8eb89d076579709ca7cb465adbe802febea2a174
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-10-09 15:52:40 +0600

    Change userimage component into username and image component where both user name and image will be present

---
+++ src/components/shared/UsernameWithImage/UsernameWithImage.jsx
@@ -0,0 +1,31 @@
+// react imports
+import PropTypes from "prop-types";
+
+//  react icon import
+import { FaUserCircle } from "react-icons/fa";
+
+const UsernameWithImage = ({
+  username = "Unknown user",
+  profilePicture = null,
+}) => {
+  return (
+    <div className="flex items-center gap-4">
+      <p className="text-white">{username}</p>
+      <div className="w-[2.5rem] aspect-square rounded-full overflow-hidden">
+        {!profilePicture && (
+          <FaUserCircle className="w-full h-full object-contain text-white"></FaUserCircle>
+        )}
+        {profilePicture !== null && (
+          <img src={profilePicture} alt="user image" />
+        )}
+      </div>
+    </div>
+  );
+};
+
+UsernameWithImage.propTypes = {
+  username: PropTypes.string,
+  profilePicture: PropTypes.string,
+};
+
+export default UsernameWithImage;
