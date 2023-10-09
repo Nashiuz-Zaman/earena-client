518a700a8463faa030d25a9b4ddd627fcdcc2359
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-10-09 15:53:11 +0600

    Change visit store option to contact us

--- src/data/navigationOptions.js
+++ src/data/navigationOptions.js
@@ -1,5 +1,5 @@
 export const navigationOptions = [
   { id: 1, text: "Home", url: "/" },
   { id: 2, text: "Tournaments", url: "/tournaments" },
-  { id: 3, text: "Visit Store", url: "/store" },
+  { id: 3, text: "Contact Us", url: "/contact" },
 ];
