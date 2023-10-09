5200b97300635c530b7b7ed1705574c639d4cff5
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-10-09 19:44:38 +0600

    Create about us page component and the heading for the page

---
+++ src/components/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,17 @@
+import SectionHeading from "../../shared/SectionHeading/SectionHeading";
+
+const AboutUs = () => {
+  return (
+    <div className="mt-sectionGapLg">
+      <SectionHeading
+        text={
+          <>
+            Meet Our <span className="text-primary">Team</span>
+          </>
+        }
+      />
+    </div>
+  );
+};
+
+export default AboutUs;
